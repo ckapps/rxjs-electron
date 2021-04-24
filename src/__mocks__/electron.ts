e61e44c52d80141f20e973e8cfa5d0bebe87dda2
christophka <chris@ckapp.net>
2021-04-24 12:30:26 +0200

    test: add unit test for app-is-ready

---
+++ src/__mocks__/electron.ts
@@ -0,0 +1,12 @@
+/**
+ * File containing mocks for electron modules
+ */
+
+// ==================================================
+// Electron: app
+// ==================================================
+export const app = {
+  whenReady: jest.fn(async () => {
+    return void 0;
+  }),
+};
---
+++ src/app/app-is-ready.spec.ts
@@ -0,0 +1,7 @@
+import { appIsReady$ } from './app-is-ready';
+
+describe('app/is-ready', () => {
+  it('should have some test', () => {
+    expect(appIsReady$).toBeDefined();
+  });
+});
